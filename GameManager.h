01e9439b12782c3632c2da4218f792e0dc059eef
datoonalthearishbsit-max <datoonalthearish_bsit@plmun.edu.ph>
2026-05-04 12:08:28 +0800

    Update and rename ceujnkm to GameManager.h

---
+++ GameManager.h
@@ -0,0 +1,16 @@
+Gamemanager.h
+
+#ifndef GAMEMANAGER_H
+#define GAMEMANAGER_H
+
+#include "SudokuBoard.h"
+
+class GameManager {
+private:
+    SudokuBoard board;
+
+public:
+    void run();
+};
+
+
